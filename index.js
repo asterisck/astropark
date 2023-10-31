de524a1c57763c000f59159c6b3785cf1509392d
Prem <89295807+asterisck@users.noreply.github.com>
2023-10-31 10:49:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const { MongoClient } = require('mongodb');
 const url = 'mongodb://viaduct.proxy.rlwy.net:17600';
 const client = new MongoClient(url);
